b97ed3702adfff5ef465420c8732fed4cfa6a8e2
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-06-19 16:36:45 +0200

    Init app

---
+++ portal.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: Portal
+Plugin URI: https://github.com/mittmedia/portal
+Description: It's a blog portal.
+Version: 1.0.0
+Author: Fredrik Sundström
+Author URI: https://github.com/fredriksundstrom
+License: MIT
+*/
+
+/*
+Copyright (c) 2012 Fredrik Sundström
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+require_once( 'wp_mvc-1.0.0/init.php' );
+
+$portal_app = new \WpMvc\Application();
+
+$portal_app->init( WP_PLUGIN_DIR . '/portal' );
+
+// WP: Add pages
+add_action( "admin_menu", "portal_add_pages" );
+function portal_add_pages()
+{
+  add_options_page( "Portal Options", "Portal", "Administrator", "portal_options_menu", "portal_options_page" );
+}
+
+function portal_options_page()
+{
+  global $portal_app;
+  
+  $portal_app->option_controller->index();
+}
