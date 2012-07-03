1ad2f8cbd2f93c3fa19ac83e1e2a48e4a8304ea6
Magnus Engström <magnus.engstrom@mittmedia.se>
2012-07-03 10:46:21 +0200

    add agreements to porta

--- controllers/options_controller.php
+++ controllers/options_controller.php
@@ -2,14 +2,16 @@
 
 namespace Portal
 {
-  class OptionsController extends \WpMvc\BaseController
+  class SettingsController extends \WpMvc\BaseController
   {
     public function index()
     {
+      global $current_site;
       global $site;
       global $theme_names;
+      global $registration_options;
 
-      $site = \WpMvc\Site::find( 1 );
+      $site = \WpMvc\Site::find( $current_site->id );
 
       $this->create_attribute_if_not_exists( $site, 'companyname' );
       $this->create_attribute_if_not_exists( $site, 'defaulttheme' );
@@ -18,25 +20,37 @@ namespace Portal
       $this->create_attribute_if_not_exists( $site, 'companycontactname' );
       $this->create_attribute_if_not_exists( $site, 'companycontactphone' );
       $this->create_attribute_if_not_exists( $site, 'companycontactemail' );
+      $this->create_attribute_if_not_exists( $site, 'activate_registration', 'a' );
+      $this->create_attribute_if_not_exists( $site, 'welcome_text' );
+      $this->create_attribute_if_not_exists( $site, 'user_agreement' );
+      $this->create_attribute_if_not_exists( $site, 'pul_agreement' );
 
       if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
         $site->takes_post( $_POST['site'] );
 
         $site->save();
+
+        static::redirect_to( "{$_SERVER['REQUEST_URI']}&portal_updated=1" );
       }
 
       $this->get_theme_names( $theme_names );
 
+      $registration_options = array(
+        'a' => \Portal\SettingsHelper::activation_option_to_text( 'a' ),
+        'b' => \Portal\SettingsHelper::activation_option_to_text( 'b' ),
+        'c' => \Portal\SettingsHelper::activation_option_to_text( 'c' )
+      );
+
       $this->render( $this, "index" );
     }
 
-    private function create_attribute_if_not_exists( &$site, $attribute )
+    private function create_attribute_if_not_exists( &$site, $attribute, $value = null )
     {
       if ( ! isset( $site->sitemeta->{$attribute} ) ) {
-        $site->sitemeta->{$attribute} = SiteMeta::virgin();
+        $site->sitemeta->{$attribute} = \WpMvc\SiteMeta::virgin();
         $site->sitemeta->{$attribute}->site_id = $site->id;
         $site->sitemeta->{$attribute}->meta_key = "$attribute";
-        $site->sitemeta->{$attribute}->meta_value = "";
+        $value ? $site->sitemeta->{$attribute}->meta_value = $value : $site->sitemeta->{$attribute}->meta_value = "";
         $site->sitemeta->{$attribute}->save();
       }
     }
@@ -45,10 +59,15 @@ namespace Portal
     {
       $themes = wp_get_themes();
 
+      $allowed_themes = \WpMvc\SiteMeta::find_by_meta_key( 'allowedthemes' );
+
+      $allowed_themes_unserialized = unserialize( $allowed_themes[0]->meta_value );
+
       $theme_names = array();
 
       foreach ( $themes as $theme_key => $theme_value ) {
-        array_push( $theme_names, $theme_key );
+        if ( in_array( $theme_key, array_keys( $allowed_themes_unserialized ) ) )
+          array_push( $theme_names, $theme_key );
       }
     }
   }
--- views/options/index.php
+++ views/options/index.php
@@ -1,11 +1,15 @@
-<?php global $site; global $theme_names; ?>
+<?php global $site; global $theme_names; global $registration_options; ?>
 
 <div class="wrap">
   <div id="icon-options-general" class="icon32"><br></div>
-  <h2><?php _e( 'Portal Options' ); ?></h2>
+  <h2><?php _e( 'Portal Settings' ); ?></h2>
   <?php
 
   $content = array(
+    array(
+      'title' => 'Company Information',
+      'type' => 'h3'
+    ),
     array(
       'title' => 'Company Name',
       'name' => $site->sitemeta->companyname->meta_key,
@@ -47,12 +51,20 @@
       'key' => 'meta_value'
     ),
     array(
+      'title' => 'Portal Settings',
+      'type' => 'h3'
+    ),
+    array(
       'title' => 'Portal Start Page',
       'name' => $site->sitemeta->portalstartpage->meta_key,
       'type' => 'text',
       'object' => $site->sitemeta->portalstartpage,
       'default_value' => $site->sitemeta->portalstartpage->meta_value,
       'key' => 'meta_value'
+    ),
+    array(
+      'title' => 'Registration Settings',
+      'type' => 'h3'
     ),
     array(
       'title' => 'Default Theme',
@@ -62,7 +74,41 @@
       'object' => $site->sitemeta->defaulttheme,
       'default_value' => $site->sitemeta->defaulttheme->meta_value,
       'key' => 'meta_value'
+    ),
+    array(
+      'title' => 'Activate Registration',
+      'name' => $site->sitemeta->activate_registration->meta_key,
+      'type' => 'select',
+      'options' => $registration_options,
+      'object' => $site->sitemeta->activate_registration,
+      'default_value' => \Portal\SettingsHelper::activation_option_to_text( $site->sitemeta->activate_registration->meta_value ),
+      'key' => 'meta_value'
+    ),
+    array(
+      'title' => 'Welcome Text',
+      'name' => $site->sitemeta->welcome_text->meta_key,
+      'type' => 'textarea',
+      'object' => $site->sitemeta->welcome_text,
+      'default_value' => $site->sitemeta->welcome_text->meta_value,
+      'key' => 'meta_value'
+    ),
+    array(
+      'title' => 'User Agreement',
+      'name' => $site->sitemeta->user_agreement->meta_key,
+      'type' => 'editor_textarea',
+      'object' => $site->sitemeta->user_agreement,
+      'default_value' => $site->sitemeta->user_agreement->meta_value,
+      'key' => 'meta_value'
+    ),
+    array(
+      'title' => 'PUL Agreement',
+      'name' => $site->sitemeta->pul_agreement->meta_key,
+      'type' => 'editor_textarea',
+      'object' => $site->sitemeta->pul_agreement,
+      'default_value' => $site->sitemeta->pul_agreement->meta_value,
+      'key' => 'meta_value'
     )
+
   );
 
   \WpMvc\FormHelper::render_form( $site, $content );
